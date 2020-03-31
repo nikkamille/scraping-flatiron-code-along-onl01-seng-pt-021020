920f01ac5a019102b4553bc525f3289a6033b7b6
Kamille Longalong <k.pagibigan@gmail.com>
2020-03-31 06:48:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,14 +13,13 @@ class Scraper
      # course.title = post.css("h2").text
       #course.schedule = post.css(".date").text
       #course.description = post.css("p").text
-  
   end
   
  
   
- # def get_courses
-    
- # end
+  def get_courses
+    self.get_page.css(".post")  
+  end
   
  # def make_courses
     

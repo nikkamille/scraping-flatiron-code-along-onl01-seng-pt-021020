69413b7f041b299279bacd108493068e8ba1f1a1
Kamille Longalong <k.pagibigan@gmail.com>
2020-03-31 05:58:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,8 +7,7 @@ require_relative './course.rb'
 class Scraper
   
   def get_page
-    doc = Nokogiri::HTML(open()) 
-    html = open()
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses")) 
   end
   
   def get_courses
